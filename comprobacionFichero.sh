2edb9296f4a41707e5c7806954934cdc006bf2f4
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-09-04 14:42:45 -0500

    finalizacion de script

---
+++ comprobacionFichero.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Intriduzca una ruta: " ruta
+
+# si la ruta o archivo introducido no existe
+if [ ! -e $ruta ]; then
+	echo "Esta no ruta existe"
+	exit 1
+fi
+
+# si es un directorio
+if [ -d $ruta ]; then
+	echo "y es un directorio"
+fi
+
+# si es un archivo y tiene permisos de escritura
+if [ -f $ruta -a -w $ruta ]; then
+	echo "y es un fichero y se puede escribir"
+fi
